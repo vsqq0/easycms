9a39f91a72dd2fddebca0c6956b3f01f8b9c3307
vsqq0 <1379393531@qq.com>
2018-04-27 15:06:07 +0800

    q

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-sudo apt install docker.io
-sudo apt install docker-compose
-sudo apt install nodejs-legacy
+# sudo apt install docker.io
+# sudo apt install docker-compose
+# sudo apt install nodejs-legacy
 sudo ./deploy.sh
 echo "successful!"
